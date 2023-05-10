db702616431ed65914d532edbe89bb25f544a046
Mateo Bonino <mbonino1810@gmail.com>
2023-05-10 03:00:43 -0300

    Handled simple GET request with callbacks

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import http from "http";
 import html from "../playground";
 import Router from "./router";
+import { HttpMethod } from './httpMethod';
 
 const PORT = 324;
 
@@ -18,6 +19,18 @@ class App {
     });
 
     this.router = new Router();
+    this.server.on('request', (req, res) => {
+      if (!Object.keys(HttpMethod).includes(req.method)){
+        res.end('Invalid HTTP Method!');
+      }
+      //res.write(`Valid request with the method: ${req.method}\n`);
+      if (!this.router.routes[req.method].includes(req.url)){
+        res.end('Invalid url!!!');
+      }
+      //res.write(`Valid url: ${req.url}`);
+      console.log(this.router.routes);
+      this.router.routes[req.method][this.router.routes[req.method].indexOf(req.url) + 1](req, res);
+    });
   }
 
   run(port: number = PORT): void {
@@ -26,7 +39,7 @@ class App {
     });
   }
 
-  get(path: string, cb: () => void) {
+  get(path: string, cb: (req: any, res: any) => void) {
     this.router.get(path, cb);
   }
 
