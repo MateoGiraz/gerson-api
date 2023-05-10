cb245d09496986ab9e67815692d3043974d920e6
Mateo Bonino <mbonino1810@gmail.com>
2023-05-10 13:09:34 -0300

    Req body parse

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 // import app from './app';
 import App from './app';
+import { inspect } from 'util';
 
 const app = new App();
 
 app.get('/', (req: any, res: any) => {
     res.end('Default route');
+    console.log();
 });
 
 app.get('/greeting', (req: any, res: any) => {
