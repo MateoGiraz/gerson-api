049da5d20340d53d205bae8935778a771c5c5d29
MateoGiraz <mateogiraz27@gmail.com>
2023-05-16 17:56:43 -0300

    gersonTest data

---
+++ src/utils/errors.ts
@@ -0,0 +1,9 @@
+const Errors = {
+    INVALID_TYPE: "Invalid type (try using 'templates' or 'static')",
+    INVALID_DIRECTORY: 'No such directory',
+    INVALID_METHOD_MESSAGE: 'Invalid Http Method',
+    INVALID_URL_MESSAGE: 'Invalid URL',
+    NOT_FOUND_ERROR_MESSAGE: 'Bad request'
+};
+
+export default Errors;
